44a327dd151be69f42d2cb89bdda4b17f41b45e2
gitaufar <zhafiraufar123@gmail.com>
2025-05-11 13:37:55 +0700

    add pdocutcard

--- app/src/main/java/com/example/raion_workshop_3/MainScreen.kt
+++ app/src/main/java/com/example/raion_workshop_3/MainScreen.kt
@@ -1,7 +1,10 @@
 package com.example.raion_workshop_3
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import coil.compose.SubcomposeAsyncImage
 
 
 @Composable
@@ -9,4 +12,21 @@ fun MainScreen(
     modifier: Modifier = Modifier
 ) {
 
+}
+
+@Composable
+fun ProductCard(
+    image: String,
+    title: String,
+    desc: String,
+){
+    Column{
+
+        SubcomposeAsyncImage(
+            model = image,
+            contentDescription = desc,
+        )
+        Text(text = title)
+
+    }
 }
