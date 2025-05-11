32639cd95d0b5f423eb60e967ce63b8f13abcee6
gitaufar <zhafiraufar123@gmail.com>
2025-05-11 11:29:17 +0700

    git ini

---
+++ app/src/main/java/com/example/raion_workshop_3/MainScreen.kt
@@ -0,0 +1,12 @@
+package com.example.raion_workshop_3
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Raion_Workshop_3"
+include(":app")
+ 
